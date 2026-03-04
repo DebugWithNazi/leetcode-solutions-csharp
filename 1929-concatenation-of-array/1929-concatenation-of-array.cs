d44f39a0752348512a57a3f683bf836ba12cbca0
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-04 23:52:49 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -3,14 +3,9 @@ public class Solution {
         var n = nums.Length;
         var ans = new int[n*2];
         
-        int i = 0;
-
-        for(; i< n; i++){
+        for(int i = 0; i< n; i++){
             ans[i] = nums[i];
-        }
-        for(int j=0; i<n*2; i++){
-            ans[i]= nums[j];
-            j++;
+            ans[i+n] = nums[i];
         }
         return ans;
 
