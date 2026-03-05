8af557290ebebc82c10400722a5cece14e397ad2
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-05 12:56:38 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
@@ -1,18 +1,28 @@
 public class Solution {
     public int MaximumWealth(int[][] accounts) {
-          int count = 0;
-          int max = 0;
+        //   int count = 0;
+        //   int max = int.MinValue;
 
-          for(int i = 0; i < accounts.Length; i++){
-            for(int j = 0; j < accounts[i].Length; j++){
-                 count += accounts[i][j];
-            }
-            if(max < count){
-                max = count;
-            }
-            count = 0;
-          }
+        //   for(int i = 0; i < accounts.Length; i++){
+        //     for(int j = 0; j < accounts[i].Length; j++){
+        //          count += accounts[i][j];
+        //     }
+        //     if(max < count){
+        //         max = count;
+        //     }
+        //     count = 0;
+        //   }
 
-          return max;
+        //   return max;
+        // ------------ another solution -----------
+        int max = int.MinValue;
+
+        for(int i =0; i< accounts.Length; i++){
+             int sum = accounts[i].Sum();
+             if(max < sum){
+                max = sum;
+             }
+        }
+        return max;
     }
 }
