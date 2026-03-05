ecdc3d0b3006171ca6650ea592f08d1eacce735b
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-05 12:36:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -1,17 +1,20 @@
 public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
         
-        int max = candies.Max();
-        var res = new List<bool>();
+        // int max = candies.Max();
+        // var res = new List<bool>();
 
-        for (int i=0; i<candies.Length; i++){
-            if(candies[i] + extraCandies >= max){
-                 res.Add(true);
-            }
-            else{
-                res.Add(false);
-            }
-        }
-        return res;
+        // for (int i=0; i<candies.Length; i++){
+        //     if(candies[i] + extraCandies >= max){
+        //          res.Add(true);
+        //     }
+        //     else{
+        //         res.Add(false);
+        //     }
+        // }
+        // return res;
+        // ---------------------another solution------------------
+        int max = candies.Max();
+        return candies.Select(x => x + extraCandies >= max).ToList();
     }
 }
