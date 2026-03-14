40edcede5c4360948b562f8b39998bccc2cdd4bc
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-14 19:49:01 +0500

    Time: 0 ms (100%), Space: 45.6 MB (81.3%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string RestoreString(string s, int[] indices) {
+        var str = new char[indices.Length];
+       for(int i=0; i<indices.Length; i++){
+        str[indices[i]] = s[i]; 
+       }
+       return new string(str);
+    }
+}
