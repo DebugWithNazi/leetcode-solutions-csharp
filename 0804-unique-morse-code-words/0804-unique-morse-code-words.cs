2e79030c3e6e7f8a089806fc5b6e2e8580f3b251
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-14 23:33:15 +0500

    Time: 2 ms (7.95%), Space: 42 MB (30.68%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public int UniqueMorseRepresentations(string[] words) {
+        var res = new Dictionary<string, string>();
+        var dict = new Dictionary<char, string>();
+            dict['a'] = ".-";
+            dict['b'] = "-...";
+            dict['c'] = "-.-.";
+            dict['d'] = "-..";
+            dict['e'] = ".";
+            dict['f'] = "..-.";
+            dict['g'] = "--.";
+            dict['h'] = "....";
+            dict['i'] = "..";
+            dict['j'] = ".---";
+            dict['k'] = "-.-";
+            dict['l'] = ".-..";
+            dict['m'] = "--";
+            dict['n'] = "-.";
+            dict['o'] = "---";
+            dict['p'] = ".--.";
+            dict['q'] = "--.-";
+            dict['r'] = ".-.";
+            dict['s'] = "...";
+            dict['t'] = "-";
+            dict['u'] = "..-";
+            dict['v'] = "...-";
+            dict['w'] = ".--";
+            dict['x'] = "-..-";
+            dict['y'] = "-.--";
+            dict['z'] = "--..";
+            
+
+            foreach(var word in words){
+                var newMap = "";
+                foreach(var chr in word){
+                   newMap += dict[chr];
+                }
+                if(!res.ContainsKey(newMap)){
+                    res[newMap] = newMap;
+                }
+            }
+            return res.Count();
+
+    }
+}
