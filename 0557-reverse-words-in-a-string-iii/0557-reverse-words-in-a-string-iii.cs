799c9fc315e76e894f390f3336801c938334ecc7
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-15 13:09:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cs
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cs
@@ -1,23 +1,28 @@
 public class Solution {
+    
     public string ReverseWords(string s) {
-        var words = s.Split(" ");
-        var lastWord = words[words.Length-1];
-        var str = "";
-        for(int i=0; i<words.Length; i++){
-            str += Reverse(words[i]);
-            if(i != words.Length - 1){
-                str += " ";
-            }
-        }
-        return str;
-    }
+      return string.Join(" ", s.Split(" ").Select(w => new string(w.Reverse().ToArray())));
 
-    public string Reverse(string s){
-        var str = "";
-        var n = s.Length;
-        for(int i =n-1; i>=0; i--){
-            str += s[i];
-        }
-        return str;
     }
+    // ------------- another solution -----------
+    // public string ReverseWords(string s) {
+    //     var words = s.Split(" ");
+    //     var str = "";
+    //     for(int i=0; i<words.Length; i++){
+    //         str += Reverse(words[i]);
+    //         if(i != words.Length - 1){
+    //             str += " ";
+    //         }
+    //     }
+    //     return str;
+    // }
+
+    // public string Reverse(string s){
+    //     var str = "";
+    //     var n = s.Length;
+    //     for(int i =n-1; i>=0; i--){
+    //         str += s[i];
+    //     }
+    //     return str;
+    // }
 }
