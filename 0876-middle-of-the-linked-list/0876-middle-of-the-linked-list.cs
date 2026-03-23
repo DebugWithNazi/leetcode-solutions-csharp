eacaa19cf87e501048088de9ea2dfa48ba543a23
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-23 22:44:06 +0500

    Time: 0 ms (100%), Space: 41.4 MB (38.08%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        var count = 0;
+        var current = head;
+        while(current != null)
+        {
+            count++;
+            current = current.next;
+        }
+        int i = 0;
+        for(; i<count/2; i++)
+        {
+            head = head.next;
+        }
+       
+        return head;
+    }
+}
