0c418fa871677760e83e7a0098c6eee0d4e2d9af
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-28 00:22:38 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -19,17 +19,17 @@ public class Solution {
         else if(list1 == null || list2 == null){
             return list1 == null ? list2 : list1;
         }
-        if(list1.val < list2.val){
-            current.next = new ListNode(list1.val);
-            current = current.next;
-            list1 = list1.next;
-        }
-        else{
-            current.next = new ListNode(list2.val);
-            current = current.next;
-            list2 = list2.next;
+        // if(list1.val < list2.val){
+        //     current.next = new ListNode(list1.val);
+        //     current = current.next;
+        //     list1 = list1.next;
+        // }
+        // else{
+        //     current.next = new ListNode(list2.val);
+        //     current = current.next;
+        //     list2 = list2.next;
 
-        }
+        // }
         
         while( list1 != null && list2 != null ){
             if(list1.val < list2.val){
