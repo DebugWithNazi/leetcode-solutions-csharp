1f992fa37b024fcaab9132734ce8f5900a1dc655
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-27 13:52:12 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0258-add-digits/0258-add-digits.cs
+++ 0258-add-digits/0258-add-digits.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public int AddDigits(int num) {
-        
-        while(num > 9){
-            var sum = 0;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
+        // ..............another solution.............
+        // while(num > 9){
+        //     var sum = 0;
                        
-            while(num > 0){
-                sum += num % 10;
-                num = num / 10;
-            }
+        //     while(num > 0){
+        //         sum += num % 10;
+        //         num = num / 10;
+        //     }
            
-            num = sum;
-        }
-        return num; 
+        //     num = sum;
+        // }
+        // return num; 
     }
 }
