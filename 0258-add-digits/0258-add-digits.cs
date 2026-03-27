fe08984d29d21eb830f7bb94b4c52316c39708e9
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-27 13:42:25 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0258-add-digits/0258-add-digits.cs
+++ 0258-add-digits/0258-add-digits.cs
@@ -2,16 +2,13 @@ public class Solution {
     public int AddDigits(int num) {
         
         while(num > 9){
-            var digits = new List<int>();
-            
+            var sum = 0;
+                       
             while(num > 0){
-                digits.Add(num % 10);
+                sum += num % 10;
                 num = num / 10;
             }
-            var sum = 0;
-            foreach(var digit in digits){
-                sum += digit;
-            }
+           
             num = sum;
         }
         return num; 
