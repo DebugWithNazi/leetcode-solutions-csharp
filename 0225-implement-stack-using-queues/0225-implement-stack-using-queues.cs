4382110f478ccc36d9df3c73736ff093437153a1
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-29 16:58:00 +0500

    Time: 1 ms (90.27%), Space: 42.9 MB (99.41%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cs
@@ -0,0 +1,46 @@
+public class MyStack {
+
+    Queue<int> q1 = new Queue<int>();
+    Queue<int> q2 = new Queue<int>();
+
+    public MyStack() {
+    }
+    
+    public void Push(int x) {
+        q2.Enqueue(x);
+
+        while(q1.Count > 0){
+            q2.Enqueue(q1.Dequeue());
+        }
+
+        var temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    
+    public int Pop() {
+        return q1.Dequeue(); 
+    }
+    
+    public int Top() {
+        return q1.Peek();
+    }
+    
+    public bool Empty() {
+        if(q1.Count == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
