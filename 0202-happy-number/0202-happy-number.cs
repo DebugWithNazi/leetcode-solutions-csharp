4dbf58fd16f017222fbfa637c5edf7671cf999fc
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-30 12:35:53 +0500

    Time: 3 ms (60.53%), Space: 31.8 MB (37.39%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        if(n == 1) return true;
+        var seen = new HashSet<int>();
+        var digit = 0;
+        while(n != 1){
+           if(seen.Contains(n)) return false;
+           seen.Add(n);
+           
+           while(n > 0){
+             digit += (n%10)*(n%10);
+             n = n/10;
+           }
+           
+           n = digit;
+           digit = 0;
+           if(n==1){
+            return true;
+           }
+        }
+        return false;
+    }
+}
