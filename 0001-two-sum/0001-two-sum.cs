fb0bea4eb88c21cb9f308e2486dcba23a2b7f844
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-03-31 13:13:44 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,12 +1,24 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        var dict = new Dictionary<int,int>();
 
-        for(int i = 0; i<nums.Length; i++){
-            for(int j = i+1; j<nums.Length; j++){
-                if(nums[i]+nums[j]==target)
-                  return [i,j];
+        for(int i = 0; i< nums.Length; i++){
+            int complement = target - nums[i];
+
+            if(dict.ContainsKey(complement)){
+                return new int[]{ dict[complement], i};
             }
+            dict[nums[i]] = i;
         }
         return [-1,-1];
+
+        // -------- another sol -------
+        // for(int i = 0; i<nums.Length; i++){
+        //     for(int j = i+1; j<nums.Length; j++){
+        //         if(nums[i]+nums[j] == target)
+        //           return [i,j];
+        //     }
+        // }
+        // return [-1,-1];
     }
 }
