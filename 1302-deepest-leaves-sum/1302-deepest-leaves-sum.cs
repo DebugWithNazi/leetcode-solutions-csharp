0ebe5b0ec9fdf45dbadeb452170471de9f3a062c
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-04-02 15:14:49 +0500

    Time: 4 ms (43%), Space: 62.9 MB (43%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int DeepestLeavesSum(TreeNode root) {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        int sum = 0;
+
+        while(queue.Count > 0)
+        {
+        sum = 0;
+        int level = queue.Count();
+ 
+        for(int i = 0; i< level; i++){
+            var node = queue.Dequeue();
+            sum += node.val;
+
+            if(node.left != null) queue.Enqueue(node.left);
+            if(node.right != null) queue.Enqueue(node.right);
+         }
+        }
+        return sum;
+    }
+
+    
+}
