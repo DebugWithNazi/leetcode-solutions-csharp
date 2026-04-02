1947612a0e27ead5bc487f8b7528df4618b7919f
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2026-04-02 18:24:20 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cs
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public int MaxIncreaseKeepingSkyline(int[][] grid) {
         int n = grid.Length; 
-        var maxRow = new int[n];
-        var maxCol = new int[n];
+        var maxRow = new int[n]; // s = O(n)
+        var maxCol = new int[n]; // s = O(n)
 
-        for(int i=0; i< n; i++){
+        for(int i=0; i< n; i++){               // t= O(n^2) 
             for(int j = 0; j<n; j++){
                 maxRow[i] = Math.Max(maxRow[i], grid[i][j]);
                 maxCol[j] = Math.Max(maxCol[j], grid[i][j]);
@@ -13,7 +13,7 @@ public class Solution {
 
         int total = 0;
 
-        for(int i=0; i< n; i++){
+        for(int i=0; i< n; i++){  //t = O(n^2)
             for(int j = 0; j<n; j++){
                 total += Math.Min(maxRow[i],maxCol[j]) - grid[i][j];
             }
@@ -21,3 +21,6 @@ public class Solution {
         return total;
     }
 }
+
+// t= O(n^2)+ O(n^2)  = O(n^2) 
+// s = O(n) + O(n) = O(n)
